d64382a4cece3121edd939232d615c1e8cfe63c2
Jorge Victoria Andreu <jvandreu@hotmail.com>
2024-06-02 14:48:22 +0200

    Spring demo

---
+++ demo/src/main/java/com/jovian/demo/controllers/HelloController.java
@@ -0,0 +1,20 @@
+package com.jovian.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("app/foo")
+    public Map<String, String> foo(){
+        Map <String, String> json = new HashMap<>();
+        json.put("message","Hola Mundo");
+        json.put("date", new Date().toString());
+        return json;
+    }
+}
