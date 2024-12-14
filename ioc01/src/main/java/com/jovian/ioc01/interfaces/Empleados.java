c6b348fb0299d6d08afd5d430f9463d185d2abe3
Jorge Victoria Andreu <jorge.victoriaandreu@mimacom.com>
2024-12-14 10:24:22 +0100

    restart

--- ioc01/src/main/java/com/jovian/ioc01/interfaces/Empleados.java
+++
@@ -1,9 +0,0 @@
-package com.jovian.ioc01.interfaces;
-
-public interface Empleados {
-
-    public String getTareas();
-
-    public String getInforme();
-
-}
--- ioc01/src/main/java/com/jovian/ioc01/interfaces/Informes.java
+++
@@ -1,5 +0,0 @@
-package com.jovian.ioc01.interfaces;
-
-public interface Informes {
-    public String getInforme();
-}
